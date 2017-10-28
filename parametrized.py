6d83b20264148ea8350dffde9532445425ecacd1
Aric Coady <aric.coady@gmail.com>
2017-10-27 17:53:37 -0700

    Simplified getting default parameters.

--- parametrized.py
+++ parametrized.py
@@ -4,16 +4,13 @@ from functools import partial
 import pytest
 
 __version__ = '0.1'
+getargspec = getattr(inspect, 'getfullargspec', inspect.getargspec)
 
 
 def parametrized(func, combine=None):
     """Decorate a function with combined parameters."""
-    if hasattr(inspect, 'signature'):
-        params = inspect.signature(func).parameters.items()
-        params = {name: param.default for name, param in params if param.default != param.empty}
-    else:  # py2
-        argspec = inspect.getargspec(func)
-        params = dict(zip(reversed(argspec.args), reversed(argspec.defaults)))
+    argspec = getargspec(func)
+    params = dict(zip(reversed(argspec.args), reversed(argspec.defaults)))
     func.__defaults__ = ()  # pytest ignores params with defaults
     if combine is None:
         args, = params.items()  # multiple keywords require combine function, e.g., zip
